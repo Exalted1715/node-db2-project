6ebd2c39766dd5ea29714862e6409e0bc5ecfb84
Mitch Hammond <mitch.b.hammond@gmail.com>
2024-04-16 09:49:04 -0600

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,32 @@
 // STRETCH
+const cars = [
+    {
+    vin: '1111111111111',
+    make: 'hyundai',
+    model: 'elentra',
+    mileage: 53456,
+    title: 'clean',
+    transmission: 'manual',
+},
+{
+    vin: '1234567891012',
+    make: 'toyota',
+    model: 'corolla',
+    mileage: 126456,
+    title: 'salvage',
+    transmission: 'automatic',
+},
+{
+    vin: '1121121112111',
+    make: 'ford',
+    model: 'bronco',
+    mileage: 18653,
+    title: 'clean',
+    transmission: 'automatic',
+},
+]
+
+exports.seed = function(knex){
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
